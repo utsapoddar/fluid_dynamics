9e068d66d2db6ba9747207d0cc826d9d665b7941
Utsa Poddar <97543305+utsapoddar@users.noreply.github.com>
2025-10-10 21:28:47 -0600

    Update eq2latex.m

--- eq2latex.m
+++ eq2latex.m
@@ -1,25 +1,4 @@
 function t = eq2latex(f,copyOpt)
-%EQ2LATEX Convert an equation to latex form.
-%  t = eq2latex(f,copyOpt) converts a matlab symbolic equation to latex form
-%  and write it on a figure window to aid visual inspection of syntax or 
-%  user errors of a matlab equation.
-%
-%  t returns a text object.
-%  f should be a symbolic expression defined correctly using sym or syms.
-%  If copyOpt=='copy', the latex string is copied to clipboard and you can
-%  paste an external app such as mathtype.
-%
-%  Developer: lazymatlabber
-%       https://www.facebook.com/lazymatlabber/
-%       https://lazymatlab.tistory.com/
-%
-%  2018.12.27. First release
-%  2019.12.01. Copy option added
-%
-%  Refs.
-%  https://kr.mathworks.com/help/symbolic/sym.latex.html
-%  https://kr.mathworks.com/help/matlab/ref/text.html
-%  https://lazymatlab.tistory.com/30
 
 narginchk(0,2);
 
@@ -41,5 +20,6 @@ t.Position(1) = -width/2;
 if nargin==2
     clipboard(copyOpt,t.String);
 end
+
 
 return
